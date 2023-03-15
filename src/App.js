b221efd554eca114146c50b3910e528d4b169152
Rocky Oberti <Rockyoberti22@gmail.com>
2023-03-15 18:11:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Navbar from "./components/navbar.jsx";
+import { Routes, Route, Outlet } from 'react-router-dom'
+import About from './pages/About';
+import Achievements from './pages/Achievements';
+import Dashboard from './pages/Dashboard';
+import Stats from './pages/Stats';
+import Play from './pages/Play';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar/>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="about" element={<About />} />
+          <Route path="stats" element={<Stats />} />
+          <Route path="achievements" element={<Achievements />} />
+          <Route path="play" element={<Play />} />
+        </Route>
+      </Routes>
+    </div>
     </div>
   );
 }
 
+function Layout(){
+  return (
+    <div>
+    <Outlet />
+    </div>
+  );
+}
 export default App;
